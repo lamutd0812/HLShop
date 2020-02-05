f8d19f9fc90c5d11d338ee748616e5e612699406
lamutd0812 <lamutd0812@outlook.com>
2020-02-05 10:02:23 +0700

    Bai 7 Trien khai repository va migration database.mp4

---
+++ HLShop.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using HLShop.Data.Infrastructure;
+using HLShop.Model.Models;
+
+namespace HLShop.Data.Repositories
+{
+    public interface IProductRepository
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
