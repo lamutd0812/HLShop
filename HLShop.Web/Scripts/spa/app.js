0e4bcdc765ce55e6b7a5e2fdb851f66149880fa8
lamutd0812 <lamutd0812@outlook.com>
2020-02-09 21:22:05 +0700

    S18: Scope, RootScope va Scope long nhau trong AngularJS

---
+++ HLShop.Web/Scripts/spa/app.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../../assets/admin/libs/angular/angular.js" />
+
+// khoi tao 1 module
+var myApp = angular.module('myModul', []);
+
+//register controller
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+myApp.controller("schoolController", schoolController);
+
+// inject $scope object to controller
+//myController.$inject = ['$scope'];
+
+//controller cho scope lồng nhau
+function schoolController($scope) {
+    $scope.message = "Hello i'm school!"
+}
+
+//declare a controller
+function studentController ($scope) {
+    $scope.message = "Hello i'm student!";
+}
+
+function teacherController($scope) {
+    $scope.message = "Hello i'm teacher!";
+}
