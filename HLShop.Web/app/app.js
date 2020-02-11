56e9366b87e218965459d00ca68ea60b4be5c53a
lamutd0812 <lamutd0812@outlook.com>
2020-02-11 21:35:28 +0700

    S22: Tạo giao diện Admin và triển khai routing

--- HLShop.Web/app/app.js
+++ HLShop.Web/app/app.js
@@ -1,1 +1,17 @@
-﻿
+﻿/// <reference path="../assets/admin/libs/angular/angular.js" />
+
+// cấu hình routing cho app chính hlshop
+(function () {
+    angular.module('hlshop', ['hlshop.products', 'hlshop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
