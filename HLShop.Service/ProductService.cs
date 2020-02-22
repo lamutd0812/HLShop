ef56c490b8f74a31a7d4747349cfcfa0aec6bcb8
lamutd0812 <lamutd0812@outlook.com>
2020-02-22 11:15:37 +0700

    S44: Hiển thị sản phẩm theo danh mục và phân trang danh sách sản phẩm.

--- HLShop.Service/ProductService.cs
+++ HLShop.Service/ProductService.cs
@@ -23,6 +23,8 @@ namespace HLShop.Service
 
         IEnumerable<Product> GetHotProduct(int top);
 
+        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, out int totalRow);
+
         Product GetById(int id);
 
         void Save();
@@ -134,7 +136,14 @@ namespace HLShop.Service
 
         public IEnumerable<Product> GetHotProduct(int top)
         {
-            return _productRepository.GetMulti(x => x.Status==true && x.HotFlag==true).OrderByDescending(x => x.CreatedDate).Take(top);
+            return _productRepository.GetMulti(x => x.Status == true && x.HotFlag == true).OrderByDescending(x => x.CreatedDate).Take(top);
+        }
+
+        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, out int totalRow)
+        {
+            var query = _productRepository.GetMulti(x => x.Status == true && x.CategoryID == categoryId);
+            totalRow = query.Count();
+            return query.Skip((page - 1)*pageSize).Take(pageSize);
         }
 
         public Product GetById(int id)
--- HLShop.Web/Controllers/ProductController.cs
+++ HLShop.Web/Controllers/ProductController.cs
@@ -3,20 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using HLShop.Common;
+using HLShop.Service;
+using HLShop.Web.Infrastructure.Core;
+using HLShop.Web.Mappings;
+using HLShop.Web.Models;
 
 namespace HLShop.Web.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
-        public ActionResult Detail(int productId)
+        private IProductService _productService;
+        private IProductCategoryService _productCategoryService;
+
+        public ProductController(IProductService productService, IProductCategoryService productCategoryService)
         {
+            this._productService = productService;
+            this._productCategoryService = productCategoryService;
+        }
+
+        // GET: Product
+        public ActionResult Detail(int productId)   
+        {
+            
             return View();
         }
 
-        public ActionResult Category(int id)
+        public ActionResult Category(int id, int page=1)
         {
-            return View();
+            int pageSize = int.Parse(ConfigHelper.GetByKey("PageSize"));
+            int totalRow = 0;
+            var listProductModel = _productService.GetListProductByCategoryIdPaging(id, page, pageSize, out totalRow);
+
+            IMapper mapper = AutoMapperConfiguration.Configure();
+            var listProductViewModel = mapper.Map<IEnumerable<ProductViewModel>>(listProductModel);
+
+            int totalPage = (int)Math.Ceiling((double) totalRow / pageSize);
+
+            var category = _productCategoryService.GetById(id);
+            ViewBag.Category = mapper.Map<ProductCategoryViewModel>(category);
+
+            var paginationSet = new PaginationSet<ProductViewModel>()
+            {
+                Items = listProductViewModel,
+                MaxPage = int.Parse(ConfigHelper.GetByKey("MaxPage")),
+                Page = page,
+                TotalCount = totalRow,
+                TotalPages = totalPage
+            };
+
+            return View(paginationSet);
         }
     }
 }
